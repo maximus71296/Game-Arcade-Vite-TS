383c3fa9ad0f9f0d6bdab2e832dfae051f34c66c
maximus71296 <rajatgautam648@gmail.com>
2024-07-06 18:01:35 +0530

    final build

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider, ColorModeContext, ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import App from './App.tsx';
 import theme from './theme.ts';
 import './index.css'
